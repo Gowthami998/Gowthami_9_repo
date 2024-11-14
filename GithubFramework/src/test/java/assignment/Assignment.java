137ce1c77ea33528d4df39cc8141c373c489d033
Gowthami <Gowthami@DESKTOP-KIRHED7>
2024-11-14 18:37:45 +0530

    Second repository

---
+++ GithubFramework/src/test/java/assignment/Assignment.java
@@ -0,0 +1,13 @@
+package assignment;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Assignment {
+	@Test
+	public void demo() {
+	Reporter.log("Welcome to advance Selenium", true);	
+		
+	}	
+
+}
