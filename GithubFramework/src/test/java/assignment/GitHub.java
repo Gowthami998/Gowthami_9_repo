c7a64be1e2702c6bbdfa7b742143bbf50e0056da
shiva <shiva@AsusVivobook>
2024-11-16 09:56:29 +0530

    enter the some change

--- GithubFramework/src/test/java/assignment/GitHub.java
+++ GithubFramework/src/test/java/assignment/GitHub.java
@@ -1,9 +1,11 @@
 package assignment;
 
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class GitHub 
 {
+	@Test
 public void forkAndPsh()
 {
 	Reporter.log("enter the some changes");
