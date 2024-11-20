688ed6155692b5552aaf15bf4a9235772e6a7e8d
Gowthami <Gowthami@DESKTOP-KIRHED7>
2024-11-20 09:25:25 +0530

    poll scm test executed

---
+++ Jenkins_Maven_Framework/src/test/java/pullscm/PollSCMTest.java
@@ -0,0 +1,12 @@
+package pullscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void scm() {
+		Reporter.log("Poll scm test executed", true);
+	}
+
+}
